00569ea2819e7e5401c98dbc26a7cf862a5c186b
shravanbhoyar1 <shravanbhoyar3507>
2024-05-17 16:19:49 +0530

    ffff

--- portaljob/src/main/java/com/example/portaljob/PortaljobApplication.java
+++ portaljob/src/main/java/com/example/portaljob/PortaljobApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortaljobApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(PortaljobApplication.class, args);
+		System.out.println(hb);
 	}
 
 }
