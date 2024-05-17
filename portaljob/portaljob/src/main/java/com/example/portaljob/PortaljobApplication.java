de815ee8ddc8217e4dc56fc28e0b7ea458172e08
shravanbhoyar1 <shravanbhoyar3507>
2024-05-17 16:11:57 +0530

    done

---
+++ portaljob/portaljob/src/main/java/com/example/portaljob/PortaljobApplication.java
@@ -0,0 +1,13 @@
+package com.example.portaljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaljobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaljobApplication.class, args);
+	}
+
+}
---
+++ portaljob/portaljob/src/test/java/com/example/portaljob/PortaljobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portaljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaljobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
